46c55a37711d8ea9c7120f8c1697bbed015bcd11
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2020-01-03 09:25:37 +0100

    fix authorize dto validation: standard implementation does not attempt to validate different groups

--- src/controllers/auth/dtos/authorize-dto.ts
+++ src/controllers/auth/dtos/authorize-dto.ts
@@ -1,12 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, ValidateIf } from 'class-validator';
 
 export class AuthorizeDto {
 
   /** Authorization code to start a new session. */
+  @ValidateIf(o => !o.jwt)
   @IsNotEmpty({ groups: ['new-session'] })
   code?: string;
 
   /** JSON Web Token (JWT) of an existing session. */
+  @ValidateIf(o => !o.code)
   @IsNotEmpty({ groups: ['existing-session'] })
   jwt?: string;
 }
