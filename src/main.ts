92e97088900f28f85be4f72cd63d97532a64b93e
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2020-01-03 09:26:19 +0100

    add cors and detailed axios exception logging

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import axios from 'axios';
+import * as AxiosError from 'axios-error';
 
 import { AppModule } from './app.module';
+import { AxiosErrorFilter } from './system/axios-error.filter';
+
+// Wrap axios errors so they can be caught by the error handler.
+axios.interceptors.response.use(response => response, error => Promise.reject(new AxiosError(error)));
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://app.localhost']
+    }
+  });
+  app.useGlobalFilters(new AxiosErrorFilter());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/system/axios-error.filter.ts
@@ -0,0 +1,19 @@
+import { ExceptionFilter, Catch, ArgumentsHost, Logger } from '@nestjs/common';
+import * as AxiosError from 'axios-error';
+
+const MAX_RESPONSE_DATA = 256;
+
+@Catch(AxiosError)
+export class AxiosErrorFilter implements ExceptionFilter {
+
+  private readonly _logger = new Logger(AxiosErrorFilter.name);
+
+  catch(exception: AxiosError, host: ArgumentsHost) {
+    this._logger.error(
+      `Unhandled ${exception}`,
+      `${exception.stack}
+RESPONSE DATA (first ${MAX_RESPONSE_DATA} characters):
+${JSON.stringify(exception.response.data).substr(0, MAX_RESPONSE_DATA)}`
+    );
+  }
+}
